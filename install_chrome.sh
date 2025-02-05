35b456248b49013775daf3b15dd16ad7a8692b95
Adithyan S Pillai <101512399+XXOriginalXX@users.noreply.github.com>
2025-02-05 06:19:50 +0530

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -ex
 
-# Install dependencies
+# Install Chromium (lighter than Google Chrome)
 apt-get update
-apt-get install -y wget unzip
-
-# Download and install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+apt-get install -y chromium-browser
 
 # Verify installation
-google-chrome --version
+chromium-browser --version
