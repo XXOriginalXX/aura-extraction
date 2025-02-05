86ddadf6481561e046361a8ad52541a1ec45b315
Adithyan S Pillai <101512399+XXOriginalXX@users.noreply.github.com>
2025-02-05 06:09:44 +0530

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+set -ex
 
 # Install dependencies
 apt-get update
 apt-get install -y wget unzip
 
-# Download & Install Chrome
+# Download and install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb || apt-get --fix-broken install -y
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Verify installation
+google-chrome --version
