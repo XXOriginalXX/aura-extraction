271725940c4da35343fb5425640f44b139bce1c1
Adithyan S Pillai <101512399+XXOriginalXX@users.noreply.github.com>
2025-02-05 21:08:25 +0530

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "Installing Chrome..."
-wget -qO- https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > google-chrome.deb
-sudo apt install -y ./google-chrome.deb
-rm google-chrome.deb
-echo "Chrome Installed!"
+apt-get update && apt-get install -y chromium
+export PATH=$PATH:/usr/bin/chromium
+python3 datafetch.py
