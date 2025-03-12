a3f73dfc95e394507e7b8f0961728a1cc898354d
Adithyan S Pillai <101512399+XXOriginalXX@users.noreply.github.com>
2025-03-12 21:50:05 +0530

    Update datafetch.py

--- datafetch.py
+++ datafetch.py
@@ -158,6 +158,6 @@ def get_attendance():
         print(f"Error: {str(e)}")
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
